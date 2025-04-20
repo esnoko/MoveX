140deb62f640c6c8598c314c23b7423747f1d196
esnoko <elvisnoko18@gmail.com>
2025-04-20 17:23:36 +0200

    Refactor project configuration and update app icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -39,7 +39,6 @@ export default function RootLayout() {
   
         <Stack>
           <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
         </Stack>
   );
 }
