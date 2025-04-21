3c5b2d003561b5fc1e07a9e804e58c59efaa9906
esnoko <elvisnoko18@gmail.com>
2025-04-21 23:53:20 +0200

    Refactor project configuration and add new tabs: Chat, Rides, Home, and Profile

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+const Layout = () => {
+
+  return (
+  
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(root)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        </Stack>
+  );
+}
+
+export default Layout;
