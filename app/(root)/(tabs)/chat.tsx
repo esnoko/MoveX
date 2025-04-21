9ec207174cb1db3327963d7b53593324c1df7f56
esnoko <elvisnoko18@gmail.com>
2025-04-21 23:42:37 +0200

    Add new tabs: Chat, Rides, Home, and Profile

---
+++ app/(root)/(tabs)/chat.tsx
@@ -0,0 +1,13 @@
+import { Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const chat = () => {
+  return (
+    <SafeAreaView>
+        <Text>Chat</Text>
+    </SafeAreaView>
+  )
+}
+
+export default chat
