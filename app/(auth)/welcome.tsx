046bff632243c65f9ff6a6b4fead2703cae036aa
esnoko <elvisnoko18@gmail.com>
2025-04-22 01:07:39 +0200

    Refactor project configuration and update welcome screen styling

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 const OnBoarding = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className='flex h-full items-center justify-between bg-white'>
         <Text>OnBoarding</Text>
     </SafeAreaView>
   )
