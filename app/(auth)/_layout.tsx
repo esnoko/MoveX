6207d9e2884a4c673976d53edfab1fe73ab9779b
esnoko <elvisnoko18@gmail.com>
2025-04-21 23:57:17 +0200

    Refactor project configuration and update authentication screens

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -6,9 +6,9 @@ const Layout = () => {
   return (
   
         <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(root)" options={{ headerShown: false }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="welcome" options={{ headerShown: false }} />
+          <Stack.Screen name="sign_up" options={{ headerShown: false }} />
+          <Stack.Screen name="sign_in" options={{ headerShown: false }} />
         </Stack>
   );
 }
---
+++ app/(auth)/sign_up.tsx
@@ -0,0 +1,13 @@
+import { Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const sign_up = () => {
+  return (
+    <SafeAreaView>
+        <Text>Sign Up</Text>
+    </SafeAreaView>
+  )
+}
+
+export default sign_up
