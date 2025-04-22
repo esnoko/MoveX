c348b0782e6074bd1360f1f4234ebe46b2e79a6e
esnoko <elvisnoko18@gmail.com>
2025-04-22 23:45:27 +0200

    Refactor project configuration and update welcome screen styling

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView className="">
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </SafeAreaView>
   );
