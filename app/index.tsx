6573f6f786cfd27edc04b37e01bdf3f963f9304d
esnoko <elvisnoko18@gmail.com>
2025-04-22 23:25:47 +0200

    Refactor project configuration and remove unused code

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView className="">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </SafeAreaView>
+  );
+}
