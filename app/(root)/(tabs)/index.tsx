a228f1db0e1da1e35c024ee6e5f4b412d61508e9
esnoko <elvisnoko18@gmail.com>
2025-04-27 03:18:37 +0200

    Refactor app layout, add RootLayout component, and update dependencies

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -9,6 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold my-10 text-lg">Welcome to the app!</Text>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
