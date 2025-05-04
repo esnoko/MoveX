19341131f6813db63f5cc2d1dda505a7a908cf6f
esnoko <elvisnoko18@gmail.com>
2025-05-04 22:58:37 +0200

    Update app/_layout.tsx: Import expo-fonts for font usage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
 import { Stack } from "expo-router";
 import "./global.css";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    
+  })
   return <Stack />;
 }
